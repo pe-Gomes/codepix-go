045e1d3883bc30284f793dd49ac954bf6222b173
Pedro Gomes <pedrocgomes@proton.me>
2023-10-18 15:41:23 -0400

    feat: add server and pix services for gRPC

---
+++ application/grpc/server.go
@@ -0,0 +1,39 @@
+package grpc
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/jinzhu/gorm"
+	"github.com/pe-gomes/codepix-go/application/grpc/pb"
+	"github.com/pe-gomes/codepix-go/application/usecases"
+	"github.com/pe-gomes/codepix-go/infra/repository"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func StartGrpcServer(database *gorm.DB, port int) {
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+
+	pixRepository := repository.PixKeyRepositoryDb{Db: database}
+	pixUseCase := usecases.PixUseCase{PixKeyRepository: pixRepository}
+	pixGrpcService := NewPixGrpcService(pixUseCase)
+	pb.RegisterPixServiceServer(grpcServer, pixGrpcService)
+
+	address := fmt.Sprintf("0.0.0.0:%d", port)
+	listerner, err := net.Listen("tcp", address)
+
+	if err != nil {
+		log.Fatal("could not start grpc server", err)
+	}
+
+	log.Printf("gRPC server has been started on port %d", port)
+
+	err = grpcServer.Serve(listerner)
+
+	if err != nil {
+		log.Fatal("could not start grpc server", err)
+	}
+}
