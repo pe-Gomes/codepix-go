0b36c7307befdd9717e725774dd6f639c5317a50
Pedro Gomes <pedrocgomes@proton.me>
2023-10-18 11:43:15 -0400

    feat: add transaction useCase

---
+++ application/usecases/transaction.go
@@ -0,0 +1,83 @@
+package usecases
+
+import (
+	"errors"
+
+	"github.com/pe-gomes/codepix-go/domain/model"
+)
+
+type TransactionUseCase struct {
+	TransactionRepository model.TransactionRepositoryInterface
+	PixKeyRepository      model.PixKeyRepositoryInterface
+}
+
+func (t *TransactionUseCase) Register(accountId string, amount float64, pixKeyTo string, pixKeyKindTo string, description string) (*model.Transaction, error) {
+	account, err := t.PixKeyRepository.FindAccount(accountId)
+	if err != nil {
+		return nil, err
+	}
+
+	pixKey, err := t.PixKeyRepository.FindKeyByKind(pixKeyKindTo, pixKeyKindTo)
+	if err != nil {
+		return nil, err
+	}
+
+	transaction, err := model.NewTransaction(pixKey, amount, account, description)
+	if err != nil {
+		return nil, err
+	}
+
+	t.TransactionRepository.Save(transaction)
+	if transaction.ID != "" {
+		return transaction, err
+	}
+
+	return nil, errors.New("unable to process this transaction")
+}
+
+func (t *TransactionUseCase) Confirm(transactionId string) (*model.Transaction, error) {
+	transaction, err := t.TransactionRepository.Find(transactionId)
+	if err != nil {
+		return nil, err
+	}
+
+	transaction.Status = model.TransactionConfirmed
+	err = t.TransactionRepository.Save(transaction)
+	if err != nil {
+		return nil, err
+	}
+
+	return transaction, err
+}
+
+func (t *TransactionUseCase) Complete(transactionId string) (*model.Transaction, error) {
+	transaction, err := t.TransactionRepository.Find(transactionId)
+	if err != nil {
+		return nil, err
+	}
+
+	transaction.Status = model.TransactionCompleted
+	err = t.TransactionRepository.Save(transaction)
+	if err != nil {
+		return nil, err
+	}
+
+	return transaction, err
+}
+
+func (t *TransactionUseCase) Error(transactionId string, reason string) (*model.Transaction, error) {
+	transaction, err := t.TransactionRepository.Find(transactionId)
+	if err != nil {
+		return nil, err
+	}
+
+	transaction.Status = model.TransactionError
+	transaction.CancelDescription = reason
+	
+	err = t.TransactionRepository.Save(transaction)
+	if err != nil {
+		return nil, err
+	}
+
+	return transaction, err
+}
